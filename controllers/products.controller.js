1f26c563581bcd4945e47427d7bc33ac6bde8294
Ricardo Maciel <ricardo.santana@linkapi.com.br>
2022-06-14 09:33:54 -0300

    Finish

---
+++ controllers/products.controller.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth.js');
+const router = express.Router();
+router.use(authMiddleware);
+const product = require('../models/product');
+
+module.exports = {
+    createProduct: async (req, res) => {
+        const { size } = req.body;
+  
+        try {
+            if (await product.findOne({ size })) {
+                return res.status(400).send({ error: 'Product already exists' });
+            } else {
+                const Products = await product.create(req.body);
+                return res.status(200).send({
+                    Products,
+                });
+            }
+        } catch (err) {
+            return res.status(400).send({ error: 'Registration failed' });
+        }
+    },
+    listProduct: async (req, res) => {
+        const products = await product.find();
+        res.send(products);
+    },
+    listProductbyId: async (req, res) => {
+        var id = req.params.id;       
+        const products = await product.findOne({_id: id});
+        res.send(products);
+    },
+};
--- routes/router.js
+++ routes/router.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const authcontroller = require('../controllers/auth.controller');
+const productController = require('../controllers/products.controller');
 const router = express.Router();
 
 // eslint-disable-next-line no-unused-vars
 const User = require('../controllers/auth.controller');
 router.post('/register', authcontroller.createUser);
 router.post('/login', authcontroller.authUser);
+
+const products = require('../controllers/products.controller');
+router.post('/products', productController.createProduct);
+router.get('/products', productController.listProduct);
+router.get('/products/:id', productController.listProductbyId);
 
 module.exports = router;
